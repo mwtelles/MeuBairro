4b35a04815f184a147a3ea5b49c7f6dfb7968372
Matheus Telles <business.mwtelles@gmail.com>
2022-09-30 12:39:40 -0300

    Drawer com as páginas de perfil, reportes e configurações

--- src/navigation/AppStack.js
+++ src/navigation/AppStack.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+import ReportsScreen from '../screens/ReportsScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -9,6 +12,9 @@ const AppStack = () => {
     return (
         <Drawer.Navigator screenOptions={{headerShown: false }}>
             <Drawer.Screen component={HomeScreen} name="Home" />
+            <Drawer.Screen component={ProfileScreen} name="Profile" />
+            <Drawer.Screen component={ReportsScreen} name="Reports" />
+            <Drawer.Screen component={SettingsScreen} name="Settings" />
         </Drawer.Navigator>
     )
 }
