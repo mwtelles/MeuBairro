cf6773e05672c3be174961f1873529c8b45f1730
Matheus Telles <business.mwtelles@gmail.com>
2022-10-01 16:24:54 -0300

    Full SignIn Form

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,47 +4,51 @@ import { Text, SafeAreaView, View, Image, TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { TextInput } from 'react-native-gesture-handler';
+import CustomButton from '../components/CustomButton';
+import InputField from '../components/InputField';
 
-const LoginScreen = ({navigation}) => {
+const LoginScreen = ({ navigation }) => {
   return (
     <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
       <View style={{ paddingHorizontal: 25 }}>
         <View style={{ alignItems: 'center' }}>
           <Image source={require('../assets/images/illustrations/login.jpg')} style={{ height: 300, width: 300 }} />
         </View>
-        <Text style={{ fontFamily: 'Roboto-Medium', fontSize: 28, fontWeight: '500', color: '#333', marginBottom: 30, textAlign:'center' }}>Login</Text>
-        <View style={{ flexDirection: 'row', borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25, alignItems: 'center' }}>
-          <MaterialIcons name="alternate-email" size={20} color="#666" style={{ marginRight: 5 }} />
-          <TextInput placeholder='Email' keyboardType='email-address' style={{ flex: 1, paddingVertical: 0, fontSize: 16 }} />
+        <Text style={{ fontFamily: 'Roboto-Medium', fontSize: 28, fontWeight: '500', color: '#333', marginBottom: 30, textAlign: 'center' }}>Login</Text>
+        <InputField
+          label={'Email'}
+          icon={<MaterialIcons name="alternate-email" size={20} color="#666" style={{ marginRight: 5 }}
+          />}
+          keyboardType='email-address'
+        />
+        <InputField
+          label={'Senha'}
+          icon={
+            <MaterialIcons name="lock-outline" size={20} color="#666" style={{ marginRight: 5 }} />
+          }
+          inputType="password"
+          fieldButtonLabel={"Esqueceu a senha?"}
+          fieldButtonFunction={() => { }}
+        />
+
+        <CustomButton label={"Login"} onPress={() => { }} />
+
+        <Text style={{ textAlign: 'center', color: '#666', marginBottom: 30 }}>Ou, conecte-se com...</Text>
+
+        <View style={{ flexDirection: 'row', justifyContent: 'space-between', marginBottom: 30 }}>
+          <TouchableOpacity onPress={() => { }} style={{ borderColor: '#4267B2', borderWidth: 2, borderRadius: 10, paddingVertical: 10, paddingHorizontal: 70, backgroundColor: '#4267B2' }}>
+            <FontAwesome name="facebook-f" size={24} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => { }} style={{ borderColor: '#DB4437', borderWidth: 2, borderRadius: 10, paddingVertical: 10, paddingHorizontal: 70, backgroundColor: '#DB4437' }}>
+            <FontAwesome name="google" size={24} color="white" />
+          </TouchableOpacity>
         </View>
-        <View style={{ flexDirection: 'row', borderBottomColor: '#ccc', borderBottomWidth: 1, paddingBottom: 8, marginBottom: 25, alignItems: 'center' }}>
-          <MaterialIcons name="lock-outline" size={20} color="#666" style={{ marginRight: 5 }} />
-          <TextInput placeholder='Senha' keyboardType='default' secureTextEntry={true} style={{ flex: 1, paddingVertical: 0, fontSize: 16 }} />
-          <TouchableOpacity>
-          <Text style={{color:'#AD40AF', fontWeight: '700'}}>Esqueceu sua senha?</Text>
-        </TouchableOpacity>
-        </View>
 
-        <TouchableOpacity onPress={() => {}} style={{backgroundColor:'#AD40AF', padding:20, borderRadius: 10, marginBottom: 30}}>
-          <Text style={{color:'white', textAlign:'center', fontWeight: '700', fontSize: 16}}>Login</Text>
-        </TouchableOpacity>
-
-        <Text style={{textAlign:'center', color:'#666',marginBottom:30}}>Ou, conecte-se com...</Text>
-
-        <View style={{flexDirection:'row', justifyContent:'space-between', marginBottom:30}}>
-        <TouchableOpacity onPress={() => {}} style={{borderColor:'#4267B2', borderWidth:2, borderRadius:10, paddingVertical:10, paddingHorizontal:70, backgroundColor:'#4267B2'}}>
-          <FontAwesome name="facebook-f" size={24} color="white" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {}} style={{borderColor:'#DB4437', borderWidth:2, borderRadius:10, paddingVertical:10, paddingHorizontal:70, backgroundColor:'#DB4437'}}>
-          <FontAwesome name="google" size={24} color="white" />
-        </TouchableOpacity>
-        </View>
-
-        <View style={{flexDirection:'row', justifyContent:'center', marginBottom:30}}>
-        <Text>Ainda não possui conta?</Text>
-        <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
-        <Text style={{color:'#AD40AF',fontWeight:'700'}}> Cadastre-se</Text>
-        </TouchableOpacity>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 30 }}>
+          <Text>Ainda não possui conta?</Text>
+          <TouchableOpacity onPress={() => navigation.navigate('SignIn')}>
+            <Text style={{ color: '#AD40AF', fontWeight: '700' }}> Cadastre-se</Text>
+          </TouchableOpacity>
         </View>
       </View>
     </SafeAreaView>
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Text, SafeAreaView, View, Image, TouchableOpacity } from 'react-native';
+import { Text, SafeAreaView, View, Image, TouchableOpacity, ScrollView } from 'react-native';
 
 import { MaterialIcons } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
 import { Ionicons } from '@expo/vector-icons';
-import { TextInput } from 'react-native-gesture-handler';
 import InputField from '../components/InputField';
 import CustomButton from '../components/CustomButton';
 
 const SignInScreen = ({ navigation }) => {
     return (
         <SafeAreaView style={{ flex: 1, justifyContent: 'center', backgroundColor: 'white' }}>
-            <View style={{ paddingHorizontal: 25 }}>
+            <ScrollView showsVerticalScrollIndicator={false} style={{ paddingHorizontal: 25 }}>
                 <View style={{ alignItems: 'center' }}>
                     <Image source={require('../assets/images/illustrations/login.jpg')} style={{ height: 300, width: 300 }} />
                 </View>
                 <Text style={{ fontFamily: 'Roboto-Medium', fontSize: 28, fontWeight: '500', color: '#333', marginBottom: 30, textAlign: 'center' }}>Cadastro</Text>
+                <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 30 }}>
+                    <Text>Já possui conta?</Text>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}> Conecte-se</Text>
+                    </TouchableOpacity>
+                </View>
+                
                 <InputField
                     label={'Nome Completo'}
                     icon={<Ionicons name="person-outline" size={20} color="#666" style={{ marginRight: 5 }} />}
@@ -40,19 +46,62 @@ const SignInScreen = ({ navigation }) => {
                     }
                     inputType="password"
                 />
+                <InputField
+                    label={'Data de Nascimento'}
+                    icon={
+                        <MaterialIcons name="calendar-today" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'CPF'}
+                    icon={
+                        <MaterialIcons name="badge" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'Estado'}
+                    icon={
+                        <MaterialIcons name="map" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'Cidade'}
+                    icon={
+                        <MaterialIcons name="location-city" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'Logradouro'}
+                    icon={
+                        <MaterialIcons name="location-history" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'CEP'}
+                    icon={
+                        <MaterialIcons name="location-searching" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'Rua'}
+                    icon={
+                        <MaterialIcons name="location-on" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
+                <InputField
+                    label={'Complemento'}
+                    icon={
+                        <MaterialIcons name="not-listed-location" size={20} color="#666" style={{ marginRight: 5 }}/>
+                    }
+                />
 
                 <CustomButton 
                     label={"Continuar"}
                     onPress={() => {}}
                 />
 
-                <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 30 }}>
-                    <Text>Já possui conta?</Text>
-                    <TouchableOpacity onPress={() => navigation.goBack()}>
-                        <Text style={{ color: '#AD40AF', fontWeight: '700' }}> Conecte-se</Text>
-                    </TouchableOpacity>
-                </View>
-            </View>
+                
+            </ScrollView>
         </SafeAreaView>
     );
 }
