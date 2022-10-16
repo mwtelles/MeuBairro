e8cd9d1b4184b20d65a64d49098e2a652a6f4814
Matheus Telles <business.mwtelles@gmail.com>
2022-10-16 14:47:19 -0300

    fix: resizeMode

--- src/screens/ReportsScreen.js
+++ src/screens/ReportsScreen.js
@@ -8,7 +8,7 @@ import CustomButton from '../components/CustomButton';
 
 const ReportsScreen = ({ navigation }) => {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'space-around' }}>
+    <SafeAreaView style={{ flex: 1}}>
       <View>
         <View style={{ backgroundColor: '#53E88B', width: '15%', padding: 15, marginLeft: 20, marginVertical: 10, borderRadius: 10, opacity: 0.8 }}>
           <TouchableOpacity onPress={() => navigation.goBack()}>
@@ -20,14 +20,22 @@ const ReportsScreen = ({ navigation }) => {
           <Text style={{ fontSize: 16, fontFamily: 'Inter-Regular', paddingRight: '30%', paddingVertical: 10 }}>Esses dados não serão exibidos no pedido para a sua anonimidade</Text>
         </View>
         <View style={{ padding: 14 }}>
-          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.2)', padding: 10, borderRadius: 8, marginBottom: 20 }}>
-            <TextInput placeholder='Localização'></TextInput>
+          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.2)', padding: 13, borderRadius: 8, marginBottom: 20 }}>
+            <TextInput 
+            placeholder='Localização'
+            editable={false}
+            ></TextInput>
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.1)', padding: 10, borderRadius: 8, marginBottom: 20 }}>
+          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.1)', padding: 13, borderRadius: 8, marginBottom: 20 }}>
             <TextInput placeholder='Selecione o tipo do problema'></TextInput>
           </TouchableOpacity>
-          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.1)', padding: 10, borderRadius: 8, marginBottom: 20 }}>
-            <TextInput placeholder='Descrição'></TextInput>
+          <TouchableOpacity style={{ backgroundColor: 'rgba(0,0,0,0.1)', paddingLeft: 13, paddingRight: 13, paddingTop: 13, paddingBottom: '10%' ,borderRadius: 8, marginBottom: 20 }}>
+            <TextInput 
+            placeholder='Descrição'
+            editable={true}
+            maxLength={80}
+            multiline={true}
+            ></TextInput>
           </TouchableOpacity>
         </View>
       </View>
