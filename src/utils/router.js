af315e23e3d96bb4aa4e0e0953c9947d73420633
Matheus Telles <business.mwtelles@gmail.com>
2022-10-18 11:13:36 -0300

    fix: Corrigindo a navegação

---
+++ src/utils/router.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavigationContainer, StackActions } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import ReportsScreen from '../screens/ReportsScreen';
+import FullReportScreen from '../screens/FullReportScreen';
+import GalleryFullView from '../screens/GalleryFullView';
+import SelectPicture from '../screens/SelectPictureScreen';
+import HomeScreen from '../screens/HomeScreen';
+
+const Stack = createNativeStackNavigator();
+
+function Routes() {
+  return (
+    <Stack.Navigator screenOptions={{headerShown: false }}>
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name="Reportar" component={ReportsScreen}/>
+        <Stack.Screen name="FullReport" component={FullReportScreen}/>
+        <Stack.Screen name="GalleryFullView" component={GalleryFullView}/>
+        <Stack.Screen name="SelectPicture" component={SelectPicture}/>
+    </Stack.Navigator>
+  )
+}
+
+export default Routes
